455716e309c0507d1f788930cd524afd31adbed8
MisterRus01 <leonardo.ugresic@gmail.com>
2024-01-27 19:59:56 +0100

    RecyclerAdapter, fetching photos and text from firebase storage into recycleradapter, both CT and T

--- app/src/main/java/com/example/csgobuildguide/CounterTerroristFragment.kt
+++ app/src/main/java/com/example/csgobuildguide/CounterTerroristFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class CounterTerroristFragment : Fragment() {
 
@@ -12,9 +16,49 @@ class CounterTerroristFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
+
+        ): View? {
+
         return inflater.inflate(R.layout.fragment_counter_terrorist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.RecyclerViewCounterTerrorist)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        getGunsInfo(recyclerView)
+    }
+
+    private fun getGunsInfo(recyclerView: RecyclerView) {
+        val gunList = mutableListOf<GunInfo>()
+        val storageReference = FirebaseStorage.getInstance().reference.child("menuCounterTerrorist")
+        storageReference.listAll().addOnCompleteListener { result ->
+            if (result.isSuccessful) {
+                val items: List<StorageReference> = result.result!!.items
+                var downloadCount = 0 // Counter to track completed download operations
+
+                items.forEachIndexed { index, item ->
+                    val itemName = removeLast4Chars(item.name)
+                    item.downloadUrl.addOnSuccessListener { uri ->
+                        gunList.add(GunInfo(uri.toString(), itemName.uppercase()))
+                    }.addOnCompleteListener {
+                        downloadCount++
+                        if (downloadCount == items.size) {
+                            // Set the adapter once all downloads are complete
+                            recyclerView.adapter = RecyclerAdapter(gunList)
+                            recyclerView.layoutManager = LinearLayoutManager(context)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun removeLast4Chars(inputString: String): String {
+        if (inputString.length <= 4) {
+            return inputString
+        } else {
+            return inputString.substring(0, inputString.length - 4)
+        }
     }
 
 }
