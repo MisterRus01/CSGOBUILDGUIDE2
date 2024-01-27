1602cdddf2e8b723c8edc3764ab94e1f32f2b642
MisterRus01 <leonardo.ugresic@gmail.com>
2024-01-27 02:10:56 +0100

    RecyclerAdapter, not full

---
+++ app/src/main/java/com/example/csgobuildguide/GunInfo.kt
@@ -0,0 +1,8 @@
+package com.example.csgobuildguide
+
+import android.widget.ImageView
+
+data class GunInfo(
+    var photoPath: String,
+    var gunType: String
+)
--- app/src/main/java/com/example/csgobuildguide/MainActivity.kt
+++ app/src/main/java/com/example/csgobuildguide/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.csgobuildguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.csgobuildguide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val counterTerroristFragment = CounterTerroristFragment()
+        val terroristFragment = TerroristFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, counterTerroristFragment)
+            addToBackStack(null)
+            commit()
+        }
+
+        binding.cterrorist.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.frameLayout, counterTerroristFragment)
+                commit()
+            }
+        }
+        binding.terrorist.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.frameLayout, terroristFragment)
+                commit()
+            }
+        }
     }
 }
