33b06c2a32c633e277ca101585b8c522e52fe20d
MisterRus01 <leonardo.ugresic@gmail.com>
2024-01-30 19:19:21 +0100

    Just need to add MyProfilActivity code, logout button and my profile button added in main activity

--- app/src/main/java/com/example/csgobuildguide/MainActivity.kt
+++ app/src/main/java/com/example/csgobuildguide/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.csgobuildguide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.csgobuildguide.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -31,6 +33,16 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.frameLayout, terroristFragment)
                 commit()
             }
+        }
+        binding.logout.setOnClickListener{
+            FirebaseAuth.getInstance().signOut();
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.myprofile.setOnClickListener{
+            val intent = Intent(this, MyProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
